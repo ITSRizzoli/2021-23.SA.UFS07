0ec18b758b21f5e0414d76a8c5e144b3c1cf495a
jimwellf <jimwellfulgencio@gmail.com>
2022-05-24 11:26:59 +0200

    fix: button import

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {Button} from "fulgencio-react/atoms/button/Button"
+import {Button} from "fulgencio-react/umd/atoms/button/Button"
 
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
-        <Button label={"sssuuuuper"} />
+        <Button label={"gasssss"} />
     </div>
 );
 
