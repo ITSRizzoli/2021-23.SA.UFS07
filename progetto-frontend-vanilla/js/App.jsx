cf25ae0a84ee93bfeec18a1f9e35aa7a7e73ec21
Your Name <you@example.com>
2022-05-27 13:01:47 +0200

    feat(components-pipeline-4): use our npm package

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import {Button} from "test-1-react-hanu/atoms/button/Button"
 
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
+        <Button label={"gasssss"} />
     </div>
 );
-
-export default App;
