dcffdf936eea84da3c48d37d85a8835cca98a33d
jimwellf <jimwellfulgencio@gmail.com>
2022-05-27 13:52:07 +0200

    fix: edited paragraph item

--- design-system/react/src/molecules/paragraph/Paragraph.jsx
+++ design-system/react/src/molecules/paragraph/Paragraph.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Text } from '../../atoms/text/Text';
-import { TextBold } from '../../atoms/text-bold/TextBold';
-import { TextItalic } from '../../atoms/text-italic/TextItalic';
-
-
-export const Paragraph = ({ text, textBold, textItalic }) => {
-  return ( <p> {text} </p>
+export const Paragraph = ( {children}) => {
+  return ( <p> {children} </p>
   );
 };
 
 Paragraph.propTypes = {
-  text: Text,
-  textBold: TextBold,
-  textItalic: TextItalic
 };
 
 Paragraph.defaultProps = {
--- design-system/react/stories/Paragraph.stories.jsx
+++ design-system/react/stories/Paragraph.stories.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import { Paragraph } from '../src/molecules/paragraph/Paragraph';
+import { Text } from '../src/atoms/text/Text'
+import { TextBold } from '../src/atoms/text-bold/TextBold'
+import { TextItalic } from '../src/atoms/text-italic/TextItalic'
 
 export default {
   title: 'Example/Molecules/Paragraph',
@@ -9,11 +12,12 @@ export default {
   },
 };
 
-const Template = (args) => <Paragraph {...args} />;
+const Template = (args) => <Paragraph {...args}>
+  <Text text={"Text"} />
+  <TextBold text={"Text Bold"} /> 
+  <TextItalic text={"Text Italic"} />
+  </Paragraph>;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  text: 'Insert text',
-  textBold: 'Insert text',
-  textItalic: 'Insert text'
 };
