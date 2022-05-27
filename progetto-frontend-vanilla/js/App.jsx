1f92fe8556cece7cd8630d8de1a634d574d65fa8
Robert Hanu <r.hanu@itsrizzoli.it>
2022-05-27 13:01:36 +0200

    fix: app jsx

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import {Button} from "test-1-hanu-r/atoms/button/Button"
 
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
+        <Button label={"gasssss"} />
     </div>
 );
 
