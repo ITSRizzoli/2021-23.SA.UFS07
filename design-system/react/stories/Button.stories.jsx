b43e24b38358ac09d5dc64fd9d4a930346663786
Gabriele <g.zulini@itsrizzoli.it>
2022-06-06 11:17:16 +0200

    feat: type buttons created

--- design-system/react/stories/Button.stories.jsx
+++ design-system/react/stories/Button.stories.jsx
@@ -15,26 +15,22 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const Reset = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
+Reset.args = {
   label: 'Button',
+  type:"reset",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
+export const Submit = Template.bind({});
+Submit.args = {
   label: 'Button',
+  type:"submit",
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
+export const Bottone = Template.bind({});
+Bottone.args = {
   label: 'Button',
+  type:"button",
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
