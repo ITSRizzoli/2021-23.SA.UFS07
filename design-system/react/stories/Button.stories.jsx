65f1c2895e0da4bdd309cd9a1af53c1c2a036661
Robert Hanu <r.hanu@itsrizzoli.it>
2022-06-06 11:17:33 +0200

    fix: buttons

--- design-system/react/stories/Button.stories.jsx
+++ design-system/react/stories/Button.stories.jsx
@@ -15,26 +15,28 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const Reset = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+Reset.args = {
   primary: true,
   label: 'Button',
+  type: 'reset',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
+export const Submit = Template.bind({});
+Submit.args = {
   label: 'Button',
+  type: 'submit',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
+export const Reset1 = Template.bind({});
+Reset1.args = {
   label: 'Button',
+  type: 'reset',
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
+export const Submit1 = Template.bind({});
+Submit1.args = {
   label: 'Button',
+  type: 'submit',
 };
