0e7ebfbc2f7f93ff85edf8893a541db7b3792773
ruby <avoliodaniele9@gmail.com>
2022-06-06 10:06:27 +0200

    fix: modify text in button

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -4,7 +4,7 @@ import {Button} from "test-1-dani/atoms/button/Button"
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
-        <Button label={"gasssss"} />
+        <Button label={"Avolio"} />
     </div>
 );
 
