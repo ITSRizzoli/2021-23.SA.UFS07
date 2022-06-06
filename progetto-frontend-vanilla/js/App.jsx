416b6049f8ee50c4822fd82bd300858818df174c
jimwellf <jimwellfulgencio@gmail.com>
2022-06-06 10:18:38 +0200

    fix: add image

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import azuki from "../img/azuki.png"
 import {Button} from "fulgencio-react/umd/atoms/button/Button"
 
 const App = () => ( 
     <div>
         <h1>Kosmos</h1>
         <Button label={"Sign up"} />
+        <img src={azuki} alt="Azuki #5558"/>
     </div>
 );
 
