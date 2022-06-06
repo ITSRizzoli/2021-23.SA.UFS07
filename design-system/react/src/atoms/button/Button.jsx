1dee9b3cf04930641b8557eee89e2c5ccdaf6b4a
Your Name <simo.marca002@gmail.com>
2022-06-06 11:23:11 +0200

    feat: added all types of button and default type

--- design-system/react/src/atoms/button/Button.jsx
+++ design-system/react/src/atoms/button/Button.jsx
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ label, onClick }) => {
+export const Button = ({ label, onClick, type }) => {
   return (
     <button
       onClick={onClick}
-      type="button"
+      type="type"
     >
       {label}
     </button>
@@ -28,4 +28,5 @@ Button.propTypes = {
 
 Button.defaultProps = {
   onClick: undefined,
+  type: "button"
 };
--- design-system/react/stories/Button.stories.jsx
+++ design-system/react/stories/Button.stories.jsx
@@ -15,15 +15,37 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const PrimaryReset = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+PrimaryReset.args = {
   primary: true,
   label: 'Button',
+  type: 'reset',
 };
-
-export const Secondary = Template.bind({});
-Secondary.args = {
+export const PrimarySubmit = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+PrimarySubmit.args = {
+  primary: true,
+  label: 'Button',
+  type: 'reset',
+};
+export const PrimaryButton = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+PrimaryButton.args = {
+  primary: true,
+  label: 'Button',
+  type: 'button',
+};
+export const SecondaryReset = Template.bind({});
+SecondaryReset.args = {
+  label: 'Button',
+};
+export const SecondarySubmit = Template.bind({});
+SecondarySubmit.args = {
+  label: 'Button',
+};
+export const SecondaryButton = Template.bind({});
+SecondaryButton.args = {
   label: 'Button',
 };
 
