30ab9aad42f64d8a980924c3467949772dac1fe9
Robert Hanu <r.hanu@itsrizzoli.it>
2022-06-10 09:11:53 +0200

    feat: added jsx files

---
+++ design-system/react/src/atoms/input/Input.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Input = ({ placeholder, onData, type }) => {
+
+  function onData (inputEvent) {
+    console.log('Input.onData', InputEvent);
+    if(typeof onData === 'function') {
+      onData(InputEvent.target.value);
+    }
+    onData(InputEvent.target.value);
+  }
+
+  return (
+    <input
+      onInput={onInput}
+      type={type}
+      placeholder={placeholder}
+    >
+    </input>
+  );
+};
+
+Input.propTypes = {
+  /**
+   * Button contents
+   */
+  placeholder: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onData: PropTypes.func,
+
+  isValid: PropTypes.bool,
+  type: PropTypes.string,
+};
+
+Input.defaultProps = {
+  type: 'text',
+  onData: undefined,
+  isValid: undefined,
+};
