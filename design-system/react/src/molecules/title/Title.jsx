4025eec75f8da04d7baa738767749a138844577a
jimwellf <jimwellfulgencio@gmail.com>
2022-06-10 11:46:29 +0200

    fix: elements

--- design-system/react/src/molecules/title/Title.jsx
+++ design-system/react/src/molecules/title/Title.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Text } from '../../atoms/text/Text';
-import { TextBold } from '../../atoms/text-bold/TextBold';
-import { TextItalic } from '../../atoms/text-italic/TextItalic';
-
-
-export const Title = ({ text }) => {
-  return ( <h1> {text} </h1>
+export const Title = ({ children }) => {
+  return (
+    <h1>
+      {children}
+    </h1>
   );
 };
 
 Title.propTypes = {
-  text: [ Text, TextBold, TextItalic]
 };
 
 Title.defaultProps = {
