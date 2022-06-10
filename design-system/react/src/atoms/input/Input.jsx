b0bec722ff4ff64fc65ca2ef8a14a564e8178114
AstrallTrinity <n.santoro@itsrizzoli.it>
2022-06-10 12:19:37 +0200

    feat: insert message corretto / incorretto

--- design-system/react/src/atoms/input/Input.jsx
+++ design-system/react/src/atoms/input/Input.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 /**
  * Primary UI component for user interaction
  */
-export const Input = ({ placeholder, onData, type }) => {
+export const Input = ({ placeholder, onData, type, isValid }) => {
     function onInput(InputEvent){
 
         console.log('Input.onInput', InputEvent);
@@ -14,14 +14,19 @@ export const Input = ({ placeholder, onData, type }) => {
 
         }
     }
-  return (
-    <input
+
+  const message = isValid ? 'valore corretto' : 'valore non corretto'
+  
+    return (
+    <>
+      <input
       onInput={onInput}
       type={type}
       placeholder={placeholder}
-    >
-      
+    >   
     </input>
+    {isValid === undefined ? null : message}
+    </>
   );
 };
 
