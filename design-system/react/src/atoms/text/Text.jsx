b84b08f51609d148c84c6c96b63a6d75a61b4822
AstrallTrinity <n.santoro@itsrizzoli.it>
2022-06-14 12:22:09 +0200

    feat: insert text

---
+++ design-system/react/src/atoms/text/Text.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Text = ({}) => {
+    return(
+        <span> ciao mondo</span>
+    )
+};
+
+Text.propTypes = {
+
+};
+
+Text.defaultProps = {
+
+};
+
+
