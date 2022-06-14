5fe8994194b15e5e3926a683a482b8797a2cb5bf
Your Name <you@example.com>
2022-06-14 12:22:58 +0200

    feat: add text

---
+++ design-system/react/src/atoms/text/Text.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Text = ({}) => {
+    return (
+        <span>Ciao Mondo!</span>
+    )
+}
+
+Text.propTypes = {
+
+};
+
+Text.defaultProps = {
+    // label: 'Button',
+    // type: 'reset',
+};
