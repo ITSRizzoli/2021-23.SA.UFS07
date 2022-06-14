f101b9e2e0f88ac9d20cca07e1bfa3dea463b23c
Gabriele <g.zulini@itsrizzoli.it>
2022-06-14 12:22:12 +0200

    feat: text created

---
+++ design-system/react/src/atoms/text/Text.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Text = ({}) => {
+    return(
+        <span>Ciao mondo</span>
+    );
+};
+
+Text.prototype={
+    
+};
+
+Text.defaultProps = {
+    
+}
