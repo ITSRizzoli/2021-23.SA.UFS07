32bfe787e4338ad69923889567c957455b78e3d6
ruby <avoliodaniele9@gmail.com>
2022-06-14 12:18:22 +0200

    fix: define text and text stories

---
+++ design-system/react/stories/Text.stories.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import { Text } from '../src/atoms/text/Text';
+
+export default {
+    title: 'Example/Atoms/Text',
+    component: Text,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        //backgroundColor: { control: 'color' },
+
+    },
+  };
+
+  const Template = (args) => <Text {...args} />;
+
+  export const Plaintext = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Plaintext.args = {
+    
+};
+
