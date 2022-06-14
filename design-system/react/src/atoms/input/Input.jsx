2cb417374122ce9c89ca0a0cd0b514a0e7f016bf
Your Name <simo.marca002@gmail.com>
2022-06-14 09:59:53 +0200

    feat: added radio

--- design-system/react/src/atoms/input/Input.jsx
+++ design-system/react/src/atoms/input/Input.jsx
@@ -4,29 +4,46 @@ import PropTypes from 'prop-types';
 /**
  * Primary UI component for user interaction
  */
-export const Input = ({ placeholder, onData, isValid, type }) => {
+export const Input = ({id, placeholder, onData, isValid, type, label }) => {
+
+  function onInput (InputEvent) {
+    console.log('Input.onData', InputEvent);
+    if(typeof onData === 'function') {
+      onData(InputEvent.target.value);
+    }
+    onData(InputEvent.target.value);
+  }
+
+  const message = isValid ? "Valore Corretto" : "Valore non Corretto"
   return (
-    <input
-      onInput={onData}
-      placeholder={placeholder}
+    <>
+      <input
+      id={id}
+      onInput={onInput}
       type={type}
+      placeholder={placeholder}
     >
     </input>
+    
+    {label === undefined ? null : <label htmlFor={id}>{label}</label>}
+    {isValid === undefined ? null : message}
+    </>
   );
 };
-function onData(InputEvent)
-{
-  console.log("input.onData", InputEvent)
-}
+
 Input.propTypes = {
-  placeholder: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
   onData: PropTypes.func,
-  isValid: PropTypes.bool.isRequired,
+  isValid: PropTypes.bool,
   type: PropTypes.string,
+  label: PropTypes.string
 };
 
 Input.defaultProps = {
-  type: "text",
-  onData: "undefined",
-  isValid: "undefined",
+  onData: undefined,
+  type: 'text',
+  placeholder: undefined,
+  isValid: undefined,
+  label: undefined
 };
