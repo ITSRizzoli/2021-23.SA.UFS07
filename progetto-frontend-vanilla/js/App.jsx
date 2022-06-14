81f269f3452baf31454eac1d8853a0f9f75adc64
AstrallTrinity <n.santoro@itsrizzoli.it>
2022-06-14 12:06:53 +0200

    feat: insert input

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Button} from "test-1-santoro-react/atoms/button/Button"
+import {Input} from "test-1-santoro-react/atoms/input/Input"
 
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
         <Button label={"gasssss"} />
+        <Input id={"input gassss"}/> 
     </div>
 );
 
