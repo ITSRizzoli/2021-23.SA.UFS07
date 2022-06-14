80018537dfe0be24e20bbb69348ad6740086af9e
Your Name <you@example.com>
2022-06-14 11:19:23 +0200

    feat: add input

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Button} from "test-1-react-hanu/atoms/button/Button"
+import {Input} from "test-1-react-hanu/atoms/input/Input"
 
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
         <Button label={"gasssss"} />
+        <Input id={'20'} />
     </div>
 );
 
